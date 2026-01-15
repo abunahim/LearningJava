2c3f12b71442e2489d5a1de865cc08395ccfffe1
abunahim <abunahim5@gmail.com>
2026-01-15 05:21:00 +0000

    Program to better understand ternary operator and printf statements.

---
+++ src/LearningJava/Temperature_Converter_Program.java
@@ -0,0 +1,26 @@
+package LearningJava;
+
+import java.util.Scanner;
+
+public class Temperature_Converter_Program {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter the temperature: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Convert to Celsius or Fahrenheit? (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temp -32) * 5 / 9 : (temp * 9 / 5);
+
+        System.out.printf("%.1f°%s", newTemp, unit);
+
+        scanner.close();
+    }
+}
