a013fed0faafbdac6ae9a738799099ba29048f83
abunahim <abunahim5@gmail.com>
2026-01-15 05:19:21 +0000

    Ternary operator is used with a condition and a question mark followed by a value, if not true then something else. Similar to if else statements.

---
+++ src/LearningJava/Ternary_Operator.java
@@ -0,0 +1,20 @@
+package LearningJava;
+
+public class Ternary_Operator {
+    public static void main(String[] args) {
+
+        /*int score = 60;
+
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        System.out.println(passOrFail);
+
+        int number = 1135;
+
+        String oddOrEven = (number % 2 == 0) ? "EVEN" : "ODD";
+        System.out.println(oddOrEven);*/
+
+        int time = 3;
+        String timeOfDay = (time < 12) ? "A.M." : "P.M.";
+        System.out.println(timeOfDay);
+    }
+}
