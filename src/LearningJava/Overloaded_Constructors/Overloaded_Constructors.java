ab5a6e09ba7a001592e2f9a46e579f9e217e7eac
abunahim <abunahim5@gmail.com>
2026-01-23 03:54:55 +0000

    Overloaded Constructors allow class to have multiple constructors with different parameter lists.

---
+++ src/LearningJava/Overloaded_Constructors/Overloaded_Constructors.java
@@ -0,0 +1,27 @@
+package LearningJava.Overloaded_Constructors;
+
+public class Overloaded_Constructors {
+    public static void main(String[] args) {
+
+        User user1 = new User("Ironman");
+        User user2 = new User("Thor", "thor1500@gmail.com");
+        User user3 = new User("Antman", "antman123@yahoo.com", 35);
+        User user4 = new User();
+
+        System.out.println(user1.username);
+        System.out.println(user1.email);
+        System.out.println(user1.age);
+
+        System.out.println(user2.username);
+        System.out.println(user2.email);
+        System.out.println(user2.age);
+
+        System.out.println(user3.username);
+        System.out.println(user3.email);
+        System.out.println(user3.age);
+
+        System.out.println(user4.username);
+        System.out.println(user4.email);
+        System.out.println(user4.age);
+    }
+}
