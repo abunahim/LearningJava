735f1eabc67d5c9308cfa4e32fdad7fa420e91e8
abunahim <abunahim5@gmail.com>
2026-02-17 07:15:15 +0000

    Add JavaScript IfStatement files

---
+++ JavaScript/ControlFlow/IfStatements/IfStatements.js
@@ -0,0 +1,69 @@
+/*let age = 13;
+
+if(age >= 18){
+    console.log("You are old enough to enter this site");
+}
+else{
+    console.log("You must be 18+ to enter this site");
+}
+
+let time = 12;
+
+if(time < 12){
+    console.log("Good Morning");
+}
+else{
+    console.log("Good Afternoon");
+}
+
+let isStudent = true;
+
+if(isStudent){
+    console.log("You are a student");
+}
+else{
+    console.log("You are not a student");
+}
+
+let age = 25;
+let hasLicense = false;
+
+if(age >= 16){
+    console.log("You are old enough to drive");
+    if(hasLicense){
+        console.log("You have youe license");
+    }
+    else{
+        console.log("You do not have your license yet!")
+    }
+}
+else{
+    console.log("You must be 16+ to have a license");
+}*/
+
+const myText = document.getElementById("myText");
+const mySubmit = document.getElementById("mySubmit");
+const resultElement = document.getElementById("resultElement");
+let age;
+
+mySubmit.onclick = function(){
+    
+    age = myText.value;
+    age = Number(age);
+
+    if(age >= 100){
+        resultElement.textContent = `You are to old to enter this site.`;
+    }
+    else if(age == 0){
+        resultElement.textContent = `You can't enter. You were just born.`;
+    }
+    else if(age >= 18){
+        resultElement.textContent = `You are old enough to enter this site.`;
+    }
+    else if(age < 0){
+        resultElement.textContent = `Your age can't be below 0.`;
+    }
+    else{
+        resultElement.textContent = `You must be 18+ to enter this site.`;
+    }
+}
