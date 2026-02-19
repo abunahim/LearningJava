76775540013fe071a72fabe78031b9bf36c2d0d7
abunahim <abunahim5@gmail.com>
2026-02-19 07:46:57 +0000

    Add JavaScript Basics files

---
+++ JavaScript/Basics/LogicalOperators/LogicalOperators.js
@@ -0,0 +1,24 @@
+/*const temp = 24;
+
+if(temp > 0 && temp <= 30){
+    console.log("The weather is Good");
+}
+else{
+    console.log("The weather is Bad");
+}
+
+if(temp <= 0 || temp > 30){
+    console.log("The weather is Bad");
+}
+else{
+    console.log("The weather is Good");
+}*/
+
+const isSunny = false;
+
+if(!isSunny){
+    console.log("It is Cloudy");
+}
+else{
+    console.log("It is Sunny");
+}
---
+++ JavaScript/Basics/StrictEquality/StrictEquality.js
@@ -0,0 +1,15 @@
+const PI = 3.14;
+
+/*if(PI === "3.14"){
+    console.log("That is PI");
+}
+else{
+    console.log("That is not PI")
+}*/
+
+if(PI !== "3.14"){
+    console.log("That is not PI");
+}
+else{
+    console.log("That is PI")
+}
